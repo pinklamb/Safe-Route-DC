a44a43c9e1df34b2dd991d6226dccbea1f7519aa
pinklamb <117957255+pinklamb@users.noreply.github.com>
2026-01-30 23:39:10 -0500

    Log DB_SERVER configuration value

--- db.js
+++ db.js
@@ -18,6 +18,8 @@ export const pool = new sql.ConnectionPool({
     options: { encrypt: false, trustServerCertificate: true }
 });
 
+
+console.log("DB_SERVER", config.DB_SERVER)
 function getLayerIdForYear(year = crimeBaseYear) {
     return crimeYearId + (year - crimeBaseYear);
 }
