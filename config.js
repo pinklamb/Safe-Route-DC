448e1b672a580e28ab85a3231c54c5ae1cdf537c
pinklamb <117957255+pinklamb@users.noreply.github.com>
2026-01-30 23:32:04 -0500

    Fixed config.js
    Load environment variables only in non-production environments.

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ import dotenv from "dotenv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.resolve(__dirname, ".env") });
+}
 
 export const config = {
   PORT: process.env.PORT || 4000,
