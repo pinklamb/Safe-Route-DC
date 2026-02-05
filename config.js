9204fe18a259c7d0af20df27ba11eb50805b9684
pinklamb <117957255+pinklamb@users.noreply.github.com>
2026-02-05 02:02:38 -0500

    Remove DATABASE from config.js
    Removed DATABASE configuration from environment variables.

--- config.js
+++ config.js
@@ -15,7 +15,6 @@ export const config = {
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_SERVER: process.env.DB_SERVER,
-  DATABASE: process.env.DATABASE,
   DB_PORT: process.env.DB_PORT,
   GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
   TEST_GM_KEY: process.env.TEST_GM_KEY
