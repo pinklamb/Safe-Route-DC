86135726c593cea3a653357e5cc4622b00fb72ba
pinklamb <117957255+pinklamb@users.noreply.github.com>
2026-05-14 22:02:17 -0400

    Refactor database connection to use createPool

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const reqLimit = 1000;
 
 
 
-export const pool = new mysql.ConnectionPool({
+export const pool = mysql.createPool({
     user: config.DB_USER,
     password: config.DB_PASSWORD,
     server: config.DB_SERVER,
