d5f2a8fa27c1844a532d6d9a1123962dfff5316e
Chanelle LeGrand <117957255+pinklamb@users.noreply.github.com>
2026-09-24 00:20:44 -0400

    Docs

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import dotenv from "dotenv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: path.resolve(__dirname, ".env") });
-}
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 
 export const config = {
   PORT: process.env.PORT || 4000,
@@ -16,8 +16,6 @@ export const config = {
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_SERVER: process.env.DB_SERVER,
   DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.DB_PORT,
-  GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
-  TEST_GM_KEY: process.env.TEST_GM_KEY
+  DB_PORT: process.env.DB_PORT
 };
 
